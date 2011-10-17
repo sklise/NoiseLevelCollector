ba0e6a688a8698a7383bd04a1b95ff73c3e72f38
Steven Klise <steven@stevenklise.com>
2011-10-17 12:05:35 -0400

    Posting array of readings

--- app.rb
+++ app.rb
@@ -28,13 +28,30 @@ end
 
 post "/:room/" do
   # params = :noise, :room
-  r = Reading.new(:time => Time.now, :room => params[:room], :noise => params[:noise])
-  if r.save
-    output = "success"
-  else
-    output = "failed"
+  @readings = params[:noise][1,params[:noise].length-2].split(',') # array of readings, ordered first to last
+  @now = Time.now # times for readings will be back-dated from right now
+  r_length = @readings.length # how many readings were sent?
+
+  interval = 1000 # millis between readings
+
+  puts @now
+  puts ""
+
+  puts "\e[0;34m#{params[:room]}\e[m"
+
+  @readings.each_with_index do |r,i|
+    reading = Reading.new(:time => @now-(r_length-i)*interval/1000, :room => params[:room], :noise => r)
+    puts "  #{reading.time}"
+    puts "  #{reading.noise}"
+    
+    if reading.save
+      puts "  \e[0;32msuccess\e[m"
+    else
+      puts "  \e[0;34mfailed\e[m"
+    end
+    puts ""
   end
-  output
+  ""
 end
 
 class Reading
