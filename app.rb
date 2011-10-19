aa00488de59da6df6a69b015ac484e3fc847821b
Steven Klise <steven@stevenklise.com>
2011-10-18 23:49:52 -0400

    Only save if between 9 and 12

--- app.rb
+++ app.rb
@@ -20,17 +20,18 @@ end
 
 get "/:room/:date.:format" do
   # :date => 20111019 (YYYYMMDD)
-  readings = Reading.all({
-    :limit => params["n_readings"].to_i,
-    :room => params["room"]
+  @readings = Reading.all({
+    :time.gt => params[:date].to_i-1,
+    :time.lt => params[:date].to_i+1,
+    :room => params[:room]
   })
   full_response = []
-  readings.each do |r|
-    single_response = {}
+  @readings.each do |r|
+    params[:format] == "json" ? single_response = {} : single_response = ""
     r.attributes.each do |k,v|
-      params[:format] == "json" ? single_response[k] = v : single_response = "#{v},"
+      params[:format] == "json" ? single_response[k] = v : single_response += "#{v},"
     end
-    full_response << (params[:format] == "json" ? single_response.to_json : single_response)
+    full_response << ((params[:format] == "json") ? single_response.to_json : single_response)
   end
   params[:format] == "json" ? "{"+full_response.join(",")+"}" : full_response.join("\n")
 end
@@ -48,19 +49,20 @@ post "/:room/" do
   puts @now
   puts ""
 
-  puts "\e[0;34m#{params[:room]}\e[m"
-
-  @readings.each_with_index do |r,i|
-    reading = Reading.new(:time => @now-(r_length-i)*interval/1000, :room => params[:room], :noise => r)
-    puts "  #{reading.time}"
-    puts "  #{reading.noise}"
+  if Time.now.hour >= 9-3 && Time.now <= 23-3
+    puts "\e[0;34m#{params[:room]}\e[m"
+    @readings.each_with_index do |r,i|
+      reading = Reading.new(:time => @now-(r_length-i)*interval/1000, :room => params[:room], :noise => r)
+      puts "  #{reading.time}"
+      puts "  #{reading.noise}"
     
-    if reading.save
-      puts "  \e[0;32msuccess\e[m"
-    else
-      puts "  \e[0;34mfailed\e[m"
+      if reading.save
+        puts "  \e[0;32msuccess\e[m"
+      else
+        puts "  \e[0;34mfailed\e[m"
+      end
+      puts ""
     end
-    puts ""
   end
   ""
 end
