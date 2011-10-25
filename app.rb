4f3d4fe008c27cd610bf4336e71056bb346e425f
Steven Klise <steven@stevenklise.com>
2011-10-24 23:18:28 -0400

    Removing useless chunks.

--- app.rb
+++ app.rb
@@ -1,9 +1,10 @@
 require 'bundler'
 Bundler.require
-require 'socket'
 
+# set up database, ENV['DATABASE_URL'] for Heroku and a file path for local development
 DataMapper.setup(:default,  ENV['DATABASE_URL'] || 'sqlite:///Users/sklise/ITP/Understanding_Networks/FloorNoise/NoiseLevelCollector/Understanding.sqlite')
 
+# Define the data class for the sound readings
 class Reading
   include DataMapper::Resource
 
@@ -13,34 +14,9 @@ class Reading
   property :room, String
 end
 
-class SavedValues
-  include DataMapper::Resource
-
-  property :id, Serial, :key => true
-  property :contents, Text
-end
-
 get "/" do
   @readings = Reading.all(:order => [:id.desc], :limit => 10)
   erb :main
-end
-
-get "/:room/:date.:format" do
-  # :date => 20111019 (YYYYMMDD)
-  @readings = Reading.all({
-    :time.gt => params[:date].to_i-1,
-    :time.lt => params[:date].to_i+1,
-    :room => params[:room]
-  })
-  full_response = []
-  @readings.each do |r|
-    params[:format] == "json" ? single_response = {} : single_response = ""
-    r.attributes.each do |k,v|
-      params[:format] == "json" ? single_response[k] = v : single_response += "#{v},"
-    end
-    full_response << ((params[:format] == "json") ? single_response.to_json : single_response)
-  end
-  params[:format] == "json" ? "{"+full_response.join(",")+"}" : full_response.join("\n")
 end
 
 post "/:room/" do
@@ -56,20 +32,18 @@ post "/:room/" do
   puts @now
   puts ""
 
-  # if Time.now.hour >= 9-3 && Time.now <= 23
-    puts "\e[0;34m#{params[:room]}\e[m"
-    @readings.each_with_index do |r,i|
-      reading = Reading.new(:time => @now-(r_length-i)*interval/1000.to_i, :room => params[:room], :noise => r)
-      puts "  #{reading.time}"
-      puts "  #{reading.noise}"
-    
-      if reading.save
-        puts "  \e[0;32msuccess\e[m"
-      else
-        puts "  \e[0;34mfailed\e[m"
-      end
-      puts ""
+  puts "\e[0;34m#{params[:room]}\e[m"
+  @readings.each_with_index do |r,i|
+    reading = Reading.new({:time => @now-(r_length-i)*interval/1000.to_i, :room => params[:room], :noise => r})
+    puts "  #{reading.time}"
+    puts "  #{reading.noise}"
+
+    if reading.save
+      puts "  \e[0;32msuccess\e[m"
+    else
+      puts "  \e[0;34mfailed\e[m"
     end
-  # end
+    puts ""
+  end
   ""
 end
